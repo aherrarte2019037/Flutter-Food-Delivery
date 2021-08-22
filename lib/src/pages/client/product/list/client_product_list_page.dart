584e12e8b4b67fe9b41adebf7d0fe0f120bfe762
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-08-21 21:00:34 -0600

    Se creo drawer cliente

--- lib/src/pages/client/product/list/client_product_list_page.dart
+++ lib/src/pages/client/product/list/client_product_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:food_delivery/src/pages/client/product/list/client_product_list_controller.dart';
 
 class ClientProductListPage extends StatefulWidget {
   const ClientProductListPage({Key? key}) : super(key: key);
@@ -8,12 +10,72 @@ class ClientProductListPage extends StatefulWidget {
 }
 
 class _ProductListState extends State<ClientProductListPage> {
+  final ClientProductListController _productListController =
+      ClientProductListController();
+
+  @override
+  void initState() {
+    SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+      _productListController.init(context);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('CLIENT PRODUCT LIST PAGE')
-      ),
+      body: Center(child: Text('CLIENT PRODUCT LIST PAGE')),
     );
   }
 }
+
+Widget userDrawer() {
+  return Drawer(
+    child: Container(
+      color: const Color.fromRGBO(50, 75, 205, 1),
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Column(
+              children: [],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildHeader({required String urlImage, required String email, required VoidCallback onClicked}) =>
+    InkWell(
+      onTap: onClicked,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 40),
+        child: Row(
+          children: [
+            CircleAvatar(radius: 30, backgroundImage: NetworkImage(urlImage)),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'asdasdasds',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  email,
+                  style: const TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ],
+            ),
+            const Spacer(),
+            const CircleAvatar(
+              radius: 24,
+              backgroundColor: Color.fromRGBO(30, 60, 168, 1),
+              child: Icon(Icons.add_comment_outlined, color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
